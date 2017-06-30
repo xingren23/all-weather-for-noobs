28a4780986e528f886fd29650184adf1ade5583e
wang <wangzg@xueqiu.com>
2017-06-30 22:30:11 +0800

    cal risk weight

--- modules/all_weather_settings.py
+++ modules/all_weather_settings.py
@@ -15,12 +15,12 @@ TICKERS = {
 }
 
 TICKER_VOLATILITY_OVERRIDES = {
-	"stocks": ['SPY', 'QQQ', 'EWJ', 'FXI'],
-	"commodities": ['DBC'],
-	"corporate credit": ['LQD'],
-	"EM credit": ['EMB'],  # empty for now, can add
-	"nominal bonds": ['TLT', 'IEF'],
-	"inflation-linked": ['GLD']
+	# "stocks": ['SPY', 'QQQ', 'EWJ', 'FXI'],
+	# "commodities": ['DBC'],
+	# "corporate credit": ['LQD'],
+	# "EM credit": ['EMB'],  # empty for now, can add
+	# "nominal bonds": ['TLT', 'IEF'],
+	# "inflation-linked": ['GLD']
 }
 OVERRIDE_TICKERS = [
 	'SPY', 'QQQ', 'EWJ', 'FXI', 'DBC', 'LQD', 'EMB', 'TLT', 'IEF', 'GLD'
