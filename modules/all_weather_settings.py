9dcb15014ca1368b09b65ab4f7412dd3ae3162b7
wang <wangzg@xueqiu.com>
2017-06-30 22:26:58 +0800

    implied vol

--- modules/all_weather_settings.py
+++ modules/all_weather_settings.py
@@ -14,7 +14,14 @@ TICKERS = {
 	"inflation-linked": ['GLD']
 }
 
-TICKER_VOLATILITY_OVERRIDES = {}
+TICKER_VOLATILITY_OVERRIDES = {
+	"stocks": ['SPY', 'QQQ', 'EWJ', 'FXI'],
+	"commodities": ['DBC'],
+	"corporate credit": ['LQD'],
+	"EM credit": ['EMB'],  # empty for now, can add
+	"nominal bonds": ['TLT', 'IEF'],
+	"inflation-linked": ['GLD']
+}
 OVERRIDE_TICKERS = [
 	'SPY', 'QQQ', 'EWJ', 'FXI', 'DBC', 'LQD', 'EMB', 'TLT', 'IEF', 'GLD'
 ]
