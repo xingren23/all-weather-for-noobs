8e05dc65f783a5520cd9d73cd653a194ab159b19
wang <wangzg@xueqiu.com>
2017-06-30 11:47:42 +0800

    1260 windows

--- all-weather.py
+++ all-weather.py
@@ -73,6 +73,7 @@ def finalize_ticker_weights(asset_class_weights, environment_weights, box_weight
 		"corporate credit": corporate_credit_weight
 	}
 
+
 	weights_by_asset_predefined = {
 		"stocks": 0.30,
 		"commodities": 0.075,
