188f74efbaf223d510d8908742dc8b02477e6c8d
wang <wangzg@xueqiu.com>
2017-07-14 14:23:06 +0800

    fix data results

--- cefx_analyse.py
+++ cefx_analyse.py
@@ -6,14 +6,16 @@ import os
 from datetime import datetime
 
 DATA_PATH = 'data/cef'
+INDEX_WEIGHTS = {'CEFIGX': 0.4, 'CEFOIX':0.3, 'CEFHYX':0.3}
 
-def cefx_merge(index, in_index, index_classes, all_cefs_pd, adjusted):
+def cefx_merge(index, in_index, index_groups, all_cefs_pd, adjusted):
     cefx_quarters = pd.read_csv('%s/%s_QUARTER_SNAPSHOT.csv' % (DATA_PATH,index))
     cefx_quarters['Ticker'] = cefx_quarters['Ticker'].apply(lambda x: x[:x.find(" ")] if x.find(" ")>0 else x)
-    if in_index != index:
-        # 只选择in_index中包括的股票代码
-        cefx_quarters = cefx_quarters.loc[cefx_quarters['Ticker'].isin(index_classes[in_index])]
     cefx_quarters.index = cefx_quarters['Ticker']
+    if index != in_index:
+        # 只选择in_index中包括的股票代码
+        in_index_pd = index_groups.get_group(in_index)
+        cefx_quarters = cefx_quarters.loc[cefx_quarters.index.isin(in_index_pd['Ticker'])]
 
     cefx_pd = pd.DataFrame()
     start_date = None
@@ -36,16 +38,17 @@ def cefx_merge(index, in_index, index_classes, all_cefs_pd, adjusted):
     returns = []
     for time,row in cefx_pd.T.iteritems():
         date = time.strftime('%Y-%m-%d')
-        # if date < '2006-04-21':
+        # if date != '2008-10-03':
         #     continue
         if date in day_groups.groups.keys():
             day_pd = day_groups.get_group(date)
             day_pd.index = day_pd['TICKER']
 
-            discount_value, full_discount_value, discount_ratio, discount_volume = cal_row_value(index, in_index, index_classes, date, row, day_pd, 'discount', adjusted)
-            preminum_value, full_preminum_value, preminum_ratio, preminum_volume = cal_row_value(index, in_index, index_classes, date, row, day_pd, 'preminum', adjusted)
-            row_value, full_value, row_ratio, row_volume = cal_row_value(index, in_index, index_classes, date, row, day_pd, '', adjusted)
-            print date, row_value, discount_value, preminum_value, row_ratio, row_volume
+            row = row[row>0]
+            discount_value, full_discount_value, discount_ratio, discount_volume = cal_row_value(index, in_index, index_groups, date, row, day_pd, 'discount', adjusted)
+            preminum_value, full_preminum_value, preminum_ratio, preminum_volume = cal_row_value(index, in_index, index_groups, date, row, day_pd, 'preminum', adjusted)
+            row_value, full_value, row_ratio, row_volume = cal_row_value(index, in_index, index_groups, date, row, day_pd, 'normal', adjusted)
+            print date, row_value, row_ratio, row_volume, discount_value, preminum_value
             returns.append({'date': date,
                             'value': row_value, 'full_value': full_value,'ratio': row_ratio, 'row_volume': row_volume,
                             'preminum_value': preminum_value, 'full_preminum_value': full_preminum_value, 'preminum_ratio': preminum_ratio, 'preminum_volume': preminum_volume,
@@ -59,33 +62,31 @@ def cefx_merge(index, in_index, index_classes, all_cefs_pd, adjusted):
         returns_pd.to_csv('%s/%s_MERGED_RETURNS.csv' % (DATA_PATH, in_index))
 
 
-def cal_row_value(index, in_index, index_classes, date, row, day_pd, sort, adjusted):
-
-    row = row[row>0]
+def cal_row_value(index, in_index, index_groups, date, row, day_pd, sort, adjusted):
     day_pd = day_pd.loc[day_pd.index.isin(row.index)]
     if sort == 'discount':
         if index == in_index:
             # 类型中性化
             data_pd = pd.DataFrame()
             total_num = len(row) * 0.3
-            for type, type_w in index_classes[in_index].iteritems():
-                type_pd = day_pd.loc[day_pd.index.isin(index_classes[type])].sort_values(by='DiscountData', ascending=True)
-                type_pd = type_pd.head(int(type_w*total_num))
+            for type, type_w in INDEX_WEIGHTS.iteritems():
+                type_pd = day_pd.loc[day_pd.index.isin(index_groups.get_group(type)['Ticker'])].sort_values(by='DiscountData', ascending=True)
+                type_pd = type_pd.head(int(type_w * total_num))
                 data_pd = data_pd.append(type_pd)
         else:
-            data_pd = day_pd.loc[day_pd.index.isin(index_classes[in_index])].sort_values(by='DiscountData', ascending=True)
+            data_pd = day_pd.loc[day_pd.index.isin(index_groups.get_group(in_index)['Ticker'])].sort_values(by='DiscountData', ascending=True)
             data_pd = data_pd.head(int(0.3*len(data_pd)))
     elif sort == 'preminum':
         if index == in_index:
             # 类型中性化
             data_pd = pd.DataFrame()
             total_num = len(row) * 0.3
-            for type, type_w in index_classes[in_index].iteritems():
-                type_pd = day_pd.loc[day_pd.index.isin(index_classes[type])].sort_values(by='DiscountData', ascending=False)
+            for type, type_w in INDEX_WEIGHTS.iteritems():
+                type_pd = day_pd.loc[day_pd.index.isin(index_groups.get_group(type)['Ticker'])].sort_values(by='DiscountData', ascending=False)
                 type_pd = type_pd.head(int(type_w*total_num))
                 data_pd = data_pd.append(type_pd)
         else:
-            data_pd = day_pd.loc[day_pd.index.isin(index_classes[in_index])].sort_values(by='DiscountData', ascending=False)
+            data_pd = day_pd.loc[day_pd.index.isin(index_groups.get_group(in_index)['Ticker'])].sort_values(by='DiscountData', ascending=False)
             data_pd = data_pd.head(int(0.3*len(data_pd)))
     else:
         data_pd = day_pd
@@ -102,9 +103,8 @@ def cal_row_value(index, in_index, index_classes, date, row, day_pd, sort, adjus
     for symbol, weight in row.iteritems():
         if symbol in data_pd.index:
             if adjusted:
-                print symbol, weight, data_pd.ix[symbol]['Adj_Percent']
                 full_value += weight * data_pd.ix[symbol]['Adj_Percent']
-                volume += weight * data_pd.ix[symbol]['Volume']
+                # volume += weight * data_pd.ix[symbol]['Volume']
 
             if symbol == 'DSU':
                 continue
@@ -119,51 +119,74 @@ def cal_row_value(index, in_index, index_classes, date, row, day_pd, sort, adjus
     volume *= 100.0 / row_weight
     return row_value, full_value, discount_ratio, volume
 
-
-def merge_all(adjusted=False):
+def load_cefs_history(adjusted):
     # load cefs history data
     all_cefs_pd = pd.DataFrame()
-    all_files = [item for item in os.listdir(DATA_PATH) if item.endswith('_YAHOO_HISTORY.csv')]
-    for item_file in all_files:
+    all_yahoo_history = '%s/%s' % (DATA_PATH,'ALL_CEFS_YAHOO.csv')
+    all_history = '%s/%s' % (DATA_PATH, 'ALL_CEFS.csv')
+    if os.path.exists(all_yahoo_history) and adjusted:
+        all_cefs_pd = pd.read_csv(all_yahoo_history)
+    elif os.path.exists(all_history) and not adjusted:
+        all_cefs_pd = pd.read_csv(all_history)
+    else:
+        all_files = [item for item in os.listdir(DATA_PATH) if item.endswith('_YAHOO_HISTORY.csv')]
+        for item_file in all_files:
 
-        symbol = item_file[:item_file.find("_")]
-        if symbol == 'BGX':
-            continue
-        data = pd.read_csv('%s/%s_HISTORY.csv' % (DATA_PATH, symbol))
-        data.index = data['DataDateJs']
+            symbol = item_file[:item_file.find("_")]
+            if symbol == 'BGX':
+                continue
+            data = pd.read_csv('%s/%s_HISTORY.csv' % (DATA_PATH, symbol))
+            data.index = data['DataDateJs']
+            if adjusted:
+                yahoo_data = pd.read_csv('%s/%s_YAHOO_HISTORY.csv' % (DATA_PATH, symbol), index_col = 'Date')
+                # yahoo_history 是日净值 -> 转换为周净值
+                yahoo_data = yahoo_data.ix[data.index]
+                data['Adj_Percent'] = yahoo_data['Adj Close'].astype(float).pct_change()
+                data['Percent'] = yahoo_data['Close'].astype(float).pct_change()
+                # yahoo 数据源 close与adj close 乱了
+                data['Volume'] = yahoo_data['Volume'].fillna(0).astype(int) * yahoo_data['Adj Close'].astype(float)
+                data = data.reset_index(drop=True)
+                all_cefs_pd = all_cefs_pd.append(data[['DataDateJs', 'TICKER', 'Percent', 'Volume', 'DiscountData', 'Adj_Percent']]).fillna(0)
+
+            else:
+                # history 是周净值
+                data['Percent'] = data['Data'].astype(float).pct_change()
+                data = data.reset_index(drop=True)
+                all_cefs_pd = all_cefs_pd.append(data[['DataDateJs', 'TICKER', 'Percent', 'DiscountData']]).fillna(0)
         if adjusted:
-            # yahoo_history 是日净值 -> 转换为周净值
-            yahoo_data = pd.read_csv('%s/%s_YAHOO_HISTORY.csv' % (DATA_PATH, symbol), index_col = 'Date')
-            data['Adj_Percent'] = yahoo_data['Adj Close'].astype(float).pct_change()
-            data['Percent'] = yahoo_data['Close'].astype(float).pct_change()
-            # yahoo 数据源 close与adj close 乱了
-            data['Volume'] = yahoo_data['Volume'].fillna(0).astype(int) * yahoo_data['Adj Close'].astype(float)
-            data = data.reset_index(drop=True)
-            all_cefs_pd = all_cefs_pd.append(data[['DataDateJs', 'TICKER', 'Percent', 'Volume', 'DiscountData', 'Adj_Percent']]).fillna(0)
-
+            all_cefs_pd[all_cefs_pd['DataDateJs']>'2001-01-01'].to_csv(all_yahoo_history)
         else:
-            # history 是周净值
-            data['Percent'] = data['Data'].astype(float).pct_change()
-            data = data.reset_index(drop=True)
-            all_cefs_pd = all_cefs_pd.append(data[['DataDateJs', 'TICKER', 'Percent', 'DiscountData']]).fillna(0)
+            all_cefs_pd[all_cefs_pd['DataDateJs']>'2001-01-01'].to_csv(all_history)
+    return all_cefs_pd
 
 
-    indexes = [ 'CEFIGX', 'CEFOIX','CEFHYX', 'CEFBLX', 'CEFX']
-    # indexes = ['CEFX']
-    index_classes = {}
-    for index in indexes:
-        if index == 'CEFX':
-            index_classes[index] = {'CEFIGX': 0.4, 'CEFOIX':0.3, 'CEFHYX':0.3}
-        else:
+def load_cefs_types():
+    index_classes_pd = pd.DataFrame()
+    cefs_type_file = '%s/CEFS_TYPES.csv' % DATA_PATH
+    if os.path.exists(cefs_type_file):
+        index_classes_pd = pd.read_csv(cefs_type_file)
+    else:
+        for index in INDEX_WEIGHTS.keys():
             in_cefx_quarters = pd.read_csv('%s/%s_QUARTER_SNAPSHOT.csv' % (DATA_PATH,index))
             in_cefx_quarters['Ticker'] = in_cefx_quarters['Ticker'].apply(lambda x: x[:x.find(" ")] if x.find(" ")>0 else x)
             in_cefx_quarters.index = in_cefx_quarters['Ticker']
-            in_cefx_quarters.drop_duplicates(inplace=True)
-            index_classes[index] = in_cefx_quarters.index
+            in_cefx_quarters = in_cefx_quarters.groupby(in_cefx_quarters.index).first()
+            in_cefx_quarters['TYPE'] = index
+            index_classes_pd = index_classes_pd.append(pd.DataFrame(in_cefx_quarters[['TYPE', 'Ticker']]))
+        index_classes_pd.index = index_classes_pd['Ticker']
+        index_classes_pd.to_csv(cefs_type_file)
+    return index_classes_pd
+
+
+def merge_all(adjusted=False):
+    # load history data
+    all_cefs_pd = load_cefs_history(adjusted)
+    # load types
+    index_types_pd = load_cefs_types()
 
     indexes = ['CEFX']
     for index in indexes:
-        cefx_merge('CEFX',index, index_classes, all_cefs_pd, adjusted)
+        cefx_merge('CEFX', index, index_types_pd.groupby('TYPE'), all_cefs_pd, adjusted)
 
 
 if __name__ == "__main__":
