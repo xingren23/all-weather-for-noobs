bfc61d3dfb504b71e1090bcc4ee98c228a8a867c
wang <wangzg@xueqiu.com>
2017-07-25 15:41:20 +0800

    data result

--- cefs_predirect.py
+++ cefs_predirect.py
@@ -15,16 +15,20 @@ def load_history():
     if os.path.exists(file):
         history_pd = pd.read_csv(file, index_col = 'DataDateJs')
     else:
-        all_files = [item for item in os.listdir(DATA_PATH) if item.endswith('_YAHOO_HISTORY.csv')]
-        for item_file in all_files:
-            symbol = item_file[:item_file.find("_")]
+        symbols = [
+            'TEI','WIW','WIA',
+            'BCX','EMD','NHS','GIM','IAE','HIO','FAM','GHY','BGR','HYT','BTZ','ISD','EHI','MSD','DSU',
+            'BHK','BDJ','HNW','DUC','FTF','VVR','PPR','HIX','JGH','CII','GDO','INB','BKT','BST','VBF','DIAX','MCN',
+            'TSI'
+        ]
+        for symbol in symbols:
+            # symbol = item_file[:item_file.find("_")]
             # print symbol
             history_file = '%s/%s_HISTORY.csv' % (DATA_PATH, symbol)
             hist_pd = pd.read_csv(history_file, index_col = 'DataDateJs')
-            hist_pd['DiscountData'] = hist_pd['DiscountData'] + 0.0001
-            hist_pd['Return'] = hist_pd['Data'].pct_change()
-
-            hist_pd['DiscountRatio1'] = (hist_pd['DiscountData'] - hist_pd['DiscountData'].shift(1))/hist_pd['DiscountData'].shift(1)
+            hist_pd['Return'] = hist_pd['Data'].pct_change() * 100
+            hist_pd = hist_pd['2016-01-01':'2017-01-01'].dropna()
+            hist_pd['DiscountRatio1'] = (hist_pd['DiscountData'] - hist_pd['DiscountData'].shift(1)) * 100/hist_pd['DiscountData'].shift(1)
             hist_pd['DiscountRatio2'] = hist_pd['DiscountRatio1'].shift(1)
             hist_pd['DiscountRatio3'] = hist_pd['DiscountRatio1'].shift(2)
             history_pd = history_pd.append(hist_pd[['TICKER','DiscountData', 'DiscountRatio1', 'DiscountRatio2', 'DiscountRatio3', 'Return']])
@@ -40,9 +44,9 @@ def history_analyse():
 
     groups = history_pd.groupby(history_pd.index)
     for symbol, group in groups:
-        mod = sm.ols(formula="Return ~ DiscountData + DiscountRatio1 + DiscountRatio2 + DiscountRatio3", data=group).fit()
+        mod = sm.ols(formula="Return ~ DiscountData", data=group).fit()
         print symbol, mod.summary()
-        break
+        # break
 
 
 if __name__ == "__main__":
